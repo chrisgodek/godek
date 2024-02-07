6a00f0c5db07c2fa0c08f324f1d7a545648e1976
Chris Godek <godek@Chriss-MacBook-Pro.local>
2024-02-07 12:33:23 -0800

    Updated many sections

---
+++ Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/restaurants">Restaurants</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
